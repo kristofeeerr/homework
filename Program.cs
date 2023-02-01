41d58dda4d32c6ea9ca35e62196d4b29907f773a
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-01 20:46:51 +0300

    homework
    work

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Anna";
+            string surname = "Ivanova";
+            int age = 22;
+            int theWight = 57;
+            float height = 1.78f;
+            char bloodTupe = 'A';
+            bool isProgrammer = true;
+            bool isTeacher = false;
+            string zodiacSign = "Libra";
+            string myHobbies = "isTravel";
+        }
+    }
+}
